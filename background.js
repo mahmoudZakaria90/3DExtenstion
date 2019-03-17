290f2819f647029d49121340ac538b2398c2b70b
Mahmoud Zakria <me@zakcoding.com>
2019-03-17 07:59:22 +0100

    updated background.js

--- background.js
+++ background.js
@@ -1,23 +1,40 @@
-chrome.runtime.onInstalled.addListener(function() {
-   chrome.bookmarks.getTree(function( bookmarks ){
-     fetch('https://zekas-51feb.firebaseio.com/allBookmarks.json', {
-       method: 'POST',
-       body: JSON.stringify( bookmarks )
-     }).then( e => console.log( e ) )
-     .catch( e => console.log( e ) )
-   })
-   chrome.tabs.onCreated.addListener(function( tab ){
-    if( tab.url === "chrome://newtab/" ) {
-       chrome.tabs.update({
-       url: "https://mahmoudzakaria90.github.io/HelloZeka/public/"
-     })
-    }
-   })
-   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher()
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
- });
+   const fetchy = function(){
+       fetch('https://zekas-51feb.firebaseio.com/structuredBookmarks.json', {
+               method: 'DELETE'
+           })
+           .then(e => {
+               chrome.bookmarks.getTree(function(bookmarks) {
+                       const collection = [];
+                       bookmarks[0].children.forEach(data => {
+                           let bookmark = {};
+                           bookmark["title"] = data.title;
+                           bookmark["children"] = data.children;
+                           collection.push(bookmark)
+                       })
+                       console.log(collection)
+                       fetch('https://zekas-51feb.firebaseio.com/structuredBookmarks.json', {
+                           method: 'POST',
+                           body: JSON.stringify(collection)
+                       })
+                   })
+                   .catch(e => console.log(e))
+           })
+   }
+   chrome.bookmarks.onCreated.addListener(fetchy);
+   chrome.bookmarks.onRemoved.addListener(fetchy);
+   
+   chrome.runtime.onInstalled.addListener(function() {
+       chrome.tabs.onCreated.addListener(function(tab) {
+           if (tab.url === "chrome://newtab/") {
+               chrome.tabs.update({
+                   url: "https://mahmoudzakaria90.github.io/HelloZeka/public/"
+               })
+           }
+       })
+       chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+           chrome.declarativeContent.onPageChanged.addRules([{
+               conditions: [new chrome.declarativeContent.PageStateMatcher()],
+               actions: [new chrome.declarativeContent.ShowPageAction()]
+           }]);
+       });
+   });
